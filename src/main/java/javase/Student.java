be380e4165546e8bc7f298193dde84090668dab5
flw <879465780@qq>
2020-07-19 18:10:54 +0800

    update 一些demo

---
+++ src/main/java/javase/Student.java
@@ -0,0 +1,32 @@
+package javase;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private EnumDemo country;
+
+    public EnumDemo getCountry() {
+        return country;
+    }
+
+    public void setCountry(EnumDemo country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/javase/VolaititleDemo.java
@@ -0,0 +1,41 @@
+package javase;
+
+
+/**
+ * 关键字：volititle
+ * 1：保证可见性
+ * 2：不保证原子性
+ * 3：禁止指令重排
+ */
+
+//如果volatile保证原子性的话预期结果是20000
+//多次运行结果都不是两万，对incrNum进行同步后结果为20000
+class MyData{
+    //AtomicInteger num = new AtomicInteger(0); 原子类可以保证线程的安全
+    int num = 0;
+
+    void incrNum() {
+        this.num++;
+    }
+}
+public class VolaititleDemo {
+
+    public static void main(String[] args) {
+        final MyData myData = new MyData();
+
+
+        for (int i = 0; i <20 ; i++) {
+            new Thread(()->{
+                for (int j = 0; j <1000 ; j++) {
+                    myData.incrNum();
+                }
+            }).start();
+        }
+        //2代表GC线程和main线程
+        while (Thread.activeCount()>2){
+            Thread.yield();
+        }
+        System.out.println("myData.num = " + myData.num);
+    }
+
+}
