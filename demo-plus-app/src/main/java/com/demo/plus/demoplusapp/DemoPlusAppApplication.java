d29673f40a2fd11ed7baf64dfa7be59a8003bb10
loading <879465780@qq.com>
2020-09-18 12:58:16 +0800

    调整项目结构

---
+++ demo-plus-app/src/main/java/com/demo/plus/demoplusapp/DemoPlusAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.plus.demoplusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPlusAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPlusAppApplication.class, args);
+    }
+
+}
