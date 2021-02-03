022f910f3801a19b0ef7041610acc9c40fe3ea39
loading <879465780@qq.com>
2021-02-03 20:48:20 +0800

    调整

--- demo-plus-app/src/main/java/com/leven/demoplus/devstg/chain/LinkedFilterChain.java
+++ demo-plus-app/src/main/java/com/leven/demoplus/devstg/chain/LinkedFilterChain.java
@@ -3,29 +3,29 @@ package com.leven.demoplus.devstg.chain;
 import java.util.List;
 
 
-public class LinkedFilterChain<ReqT,RepD> implements IFilterChain<ReqT,RepD> {
+public class LinkedFilterChain<T, R> implements IFilterChain<T, R> {
 
-    List<IFilter<ReqT,RepD>> filterChain;
+    List<IFilter<T, R>> filterChain;
 
     @Override
-    public void doFilter(ReqT req,RepD rep) {
-        for (IFilter<ReqT, RepD> filterChain : filterChain) {
+    public void doFilter(T req, R rep) {
+        for (IFilter<T, R> filterChain : filterChain) {
             //filterChain.doFilter(req,rep);
         }
     }
 
     @Override
-    protected LinkedFilterChain<ReqT,RepD> clone() throws CloneNotSupportedException {
-        LinkedFilterChain<ReqT,RepD> cloneChain = new LinkedFilterChain<ReqT,RepD>();
+    protected LinkedFilterChain<T, R> clone() throws CloneNotSupportedException {
+        LinkedFilterChain<T, R> cloneChain = new LinkedFilterChain<T, R>();
         cloneChain.setFilterChains(filterChain);
         return cloneChain;
     }
 
-    public List<IFilter<ReqT, RepD>> getFilterChains() {
+    public List<IFilter<T, R>> getFilterChains() {
         return filterChain;
     }
 
-    public void setFilterChains(List<IFilter<ReqT, RepD>> filterChains) {
+    public void setFilterChains(List<IFilter<T, R>> filterChains) {
         this.filterChain = filterChains;
     }
 }
